918b1b1d942ef32c9bec0f75356c230bd7eda494
Darshan Ajudiya <darshanajudiya07@gmail.com>
2026-02-08 22:15:03 +0530

    create your own react lib or JSX half video complete

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div><h1>Custom React App</h1></div>
+  )
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-    <App />
-    
+    <MyApp />
   </StrictMode>,
 )
